1d9ba9bee6fd17107e85878071a3d194193a369e
Aratz M. Lasa <aratzml@opendeusto.es>
2020-06-22 12:53:33 +0200

    Added PORT and IP environments for gunicorn

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+if [ -z "${PORT}" ]; then
+  PORT=8080
+fi
+
+if [ -z "${IP}" ]; then
+  IP="0.0.0.0"
+fi
+
 ORIGIN_DIRECTORTY=$(pwd)
 BASEDIR=$(dirname "$0")
 {
   cd "$BASEDIR"
-  gunicorn toad_api.main:toad_api_app --bind 0.0.0.0:8080 --worker-class aiohttp.GunicornWebWorker
+  gunicorn toad_api.main:toad_api_app --bind $IP:$PORT --worker-class aiohttp.GunicornWebWorker
 } ||{
   :
 }
